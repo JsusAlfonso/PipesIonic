663ff36afdb51012d4c981b745f3686307c54906
= <=>
2020-03-04 09:24:35 -0600

    primer Commit pipes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { ForNumberPipe } from '../pipes/for-number.pipe';
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, ForNumberPipe]
 })
 export class HomePageModule {}
